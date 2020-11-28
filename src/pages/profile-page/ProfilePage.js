475490e0e68ffea3c9e9b3d37113eac0ae78644a
Rajput-Suraj <surajarajput@gmail.com>
2020-11-28 13:46:24 +0530

    Profile page styling.

--- src/pages/profile-page/ProfilePage.js
+++ src/pages/profile-page/ProfilePage.js
@@ -25,6 +25,41 @@ const ProfilePage = () => {
                     </div>
                 </div>
                 <hr />
+                <div className="user__body">
+                    <div className="user__body__left">
+                        <Avatar className="user__avatar" />
+                        <p className="user__text">Username</p>
+                        <div className="user__info">
+                            <p>
+                                <span className="user__info__title">
+                                    Username :
+                                </span>
+                                John
+                            </p>
+                            <p>
+                                <span className="user__info__title">
+                                    e-mail :
+                                </span>
+                                john@gmail.com
+                            </p>
+                            <p>
+                                <span className="user__info__title">
+                                    Phone :
+                                </span>
+                                1234567890
+                            </p>
+                            <p>
+                                <span className="user__info__title">
+                                    Website :
+                                </span>
+                                john.com
+                            </p>
+                        </div>
+                    </div>
+                    <div className="user__body__right">
+                        <p>Address</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
